8aec0c8697c2024292b6fc45a9524e8631122bb8
bodamanikanta <141416332+bodamanikanta@users.noreply.github.com>
2023-08-05 18:10:08 +0530

    sum to add

--- array.c
+++ array.c
@@ -2,11 +2,11 @@
 int main()
 {
 	int a[]={1,2,3,4,5};
-	int i,sum=0;
+	int i,add=0;
 	for(i=0;i<5;i++)
 	{
-		sum+=a[i];
+		add+=a[i];
 	}
-	printf("sum of array =%d",sum);
+	printf("sum of array =%d",add);
 	return 0;
 }	
